2b3b40d4d0544db77099eae89bd34a14dc701945
KrzysztofPardel <pardelkrzysztof97@gmail.com>
2023-04-19 14:21:34 +0200

    Application steps finished

--- src/pages/Donate/Application.tsx
+++ src/pages/Donate/Application.tsx
@@ -78,7 +78,7 @@ const Application = () => {
               </div>
             </div> */}
             {/* Step 3 */}
-            <div className="application-location_container">
+            {/* <div className="application-location_container">
               <h2 className="application-location_header">Location:</h2>
               <select className="application-location_select">
                 <option className="application-location_option">Choose</option>
@@ -116,9 +116,119 @@ const Application = () => {
                 Enter the name of a particular organization (optional)
               </h3>
               <input type="text" className="application-organization_input" />
-            </div>
-
+            </div> */}
             {/* Step 4 */}
+            <div className="application-delivery_container">
+              <h2 className="application-delivery_header">
+                Give us the address that you want the courier to go to:
+              </h2>
+              <div className="application-delivery_both_containers">
+                <div className="application-delivery_left_container">
+                  <h3 className="application-delivery_subheader">
+                    Enter the collection address:
+                  </h3>
+                  <div className="application-delivery_small_container">
+                    <label
+                      htmlFor="application_delivery_input_street"
+                      className="application-delivery_label"
+                    >
+                      {" "}
+                      Street
+                    </label>
+                    <input
+                      type="text"
+                      className="application-delivery_input"
+                      id="application_delivery_input_street"
+                    />
+                  </div>
+                  <div className="application-delivery_small_container">
+                    <label
+                      htmlFor="application_delivery_input_city"
+                      className="application-delivery_label"
+                    >
+                      City
+                    </label>
+                    <input
+                      type="text"
+                      className="application-delivery_input"
+                      id="application_delivery_input_city"
+                    />
+                  </div>
+                  <div className="application-delivery_small_container">
+                    <label
+                      htmlFor="application_delivery_input_zip"
+                      className="application-delivery_label"
+                    >
+                      {" "}
+                      Zip Code
+                    </label>
+                    <input
+                      type="text"
+                      className="application-delivery_input"
+                      id="application_delivery_input_zip"
+                    />
+                  </div>
+                  <div className="application-delivery_small_container">
+                    <label
+                      htmlFor="application_delivery_input_phone"
+                      className="application-delivery_label"
+                    >
+                      {" "}
+                      Phone number
+                    </label>
+                    <input
+                      type="text"
+                      className="application-delivery_input"
+                      id="application_delivery_input_phone"
+                    />
+                  </div>
+                </div>
+                <div className="application-delivery_right_container">
+                  <h3 className="application-delivery_subheader">
+                    Enter the collection date:
+                  </h3>
+                  <div className="application-delivery_small_container">
+                    <label
+                      htmlFor="application_delivery_input_date"
+                      className="application-delivery_label"
+                    >
+                      {" "}
+                      Date
+                    </label>
+                    <input
+                      type="date"
+                      className="application-delivery_input"
+                      id="application_delivery_input_date"
+                    />
+                  </div>
+                  <div className="application-delivery_small_container">
+                    <label
+                      htmlFor="application_delivery_input_time"
+                      className="application-delivery_label"
+                    >
+                      {" "}
+                      Time
+                    </label>
+                    <input
+                      type="time"
+                      className="application-delivery_input"
+                      id="application_delivery_input_time"
+                    />
+                  </div>
+                  <label
+                    htmlFor="application_delivery_textarea_info"
+                    className="application-delivery_label"
+                  >
+                    {" "}
+                    Additional information for the courier
+                    <textarea
+                      id="application_delivery_textarea_info"
+                      className="application-delivery_textarea"
+                    />
+                  </label>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="application-button_container">
             <button type="button" className="application-button">
