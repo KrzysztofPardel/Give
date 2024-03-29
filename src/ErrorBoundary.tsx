3fd96291da1885f4355634f223c7d04aa989c144
Krzysztof <pardelkrzysztof97@gmail.com>
2024-03-29 15:55:52 +0100

    Errors modified

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -1,9 +1,5 @@
 import { Component, ErrorInfo, ReactNode } from "react";
-import Error400 from "./Errors/error400";
-import Error401 from "./Errors/error401";
-import Error403 from "./Errors/error403";
-import Error405 from "./Errors/error405";
-import Error500 from "./Errors/Error500";
+import Error from "Errors/Error";
 import DefaultError from "./Errors/DefaultError";
 
 interface Props {
@@ -43,18 +39,85 @@ class ErrorBoundary extends Component<Props, State> {
   }
 
   public render() {
+    const errorElements = {
+      error400: {
+        type: 400,
+        description:
+          "The server could not understand the request due to malformed syntax or invalid parameters",
+        hint: `Check the data you're sending in the request. Ensure all required parameters are included and properly formatted.`
+      },
+      error401: {
+        type: 401,
+        description:
+          "The request lacks valid authentication credentials or the provided credentials are insufficient.",
+        hint: `Verify that you are using the correct authentication credentials. If
+        you don't have access, contact the system administrator.`
+      },
+      error403: {
+        type: 403,
+        description:
+          " The server understood the request, but you do not have the necessary permissions to access the requested resource.",
+        hint: `Make sure you have the appropriate permissions for the requested
+        action. If the issue persists, contact the system administrator.`
+      },
+      error405: {
+        type: 405,
+        description:
+          "The request method is not supported for the requested resource.",
+        hint: `Check the HTTP method used in your request. Ensure it aligns with the
+        allowed methods for the specific endpoint.`
+      },
+      error500: {
+        type: 500,
+        description:
+          "The server encountered an unexpected condition that prevented it from fulfilling the request.",
+        hint: `This is an internal server issue. While there's nothing you can do on
+        your end, you may want to report the issue to the system administrator
+        or support team for resolution.`
+      }
+    };
     if (this.state.hasError) {
       switch (this.state.errorType) {
         case "error400":
-          return <Error400 />;
+          return (
+            <Error
+              type={errorElements.error400.type}
+              description={errorElements.error400.description}
+              hint={errorElements.error400.hint}
+            />
+          );
         case "error401":
-          return <Error401 />;
+          return (
+            <Error
+              type={errorElements.error401.type}
+              description={errorElements.error401.description}
+              hint={errorElements.error401.hint}
+            />
+          );
         case "error403":
-          return <Error403 />;
+          return (
+            <Error
+              type={errorElements.error403.type}
+              description={errorElements.error403.description}
+              hint={errorElements.error403.hint}
+            />
+          );
         case "error405":
-          return <Error405 />;
+          return (
+            <Error
+              type={errorElements.error405.type}
+              description={errorElements.error405.description}
+              hint={errorElements.error405.hint}
+            />
+          );
         case "error500":
-          return <Error500 />;
+          return (
+            <Error
+              type={errorElements.error500.type}
+              description={errorElements.error500.description}
+              hint={errorElements.error500.hint}
+            />
+          );
         default:
           return <DefaultError />;
       }
--- src/Errors/error.tsx
+++ src/Errors/error.tsx
@@ -1,25 +1,28 @@
 import "./errors.scss";
 import Decoration from "../assets/Decoration.svg";
 
-const Error405 = () => {
+interface ErrorTypes {
+  type: number;
+  description: string;
+  hint: string;
+}
+
+const Error = ({ type, description, hint }: ErrorTypes) => {
   return (
     <div className="error-container">
       <div className="error-smaller_container">
-        <h1 className="error-title">Error 405</h1>
+        <h1 className="error-title">Error {type}</h1>
         <div className="donate-text_decoration">
           <img src={Decoration} alt="box" className="donate-decoration_image" />
         </div>
-        <p className="error-description">
-          The request method is not supported for the requested resource.
-        </p>
+        <p className="error-description">{description}</p>
         <p className="error-hint">
           <span className="error-hint-b">HINT:</span>
-          Check the HTTP method used in your request. Ensure it aligns with the
-          allowed methods for the specific endpoint.
+          {hint}
         </p>
       </div>
     </div>
   );
 };
 
-export default Error405;
+export default Error;
